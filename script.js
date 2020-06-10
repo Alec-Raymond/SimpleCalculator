daecdb99ae2a2eb193c5b107eff0c0388275185e
Alec-Raymond <65430153+Alec-Raymond@users.noreply.github.com>
2020-06-10 05:36:33 +0000

    removed the redundant parser function, and made the numeric input much more simple

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let operand1 = 0.0,
-  operand2 = "0";
+  operand2 = "";
 let operator = "";
 
 // Simulating the screen of a calculator
@@ -12,20 +12,12 @@ const displayOperand1 = function () {
 
 // Displays operand2, a string
 const displayOperand2 = function () {
-  if (operand2 == "0") outputLine.innerHTML = 0;
-  else if (operand2.slice(0, 1) == "-")
-    outputLine.innerHTML = "-" + operand2.slice(2);
-  else outputLine.innerHTML = operand2.slice(1);
+  if (operand2 == "") outputLine.innerHTML = 0;
+  else outputLine.innerHTML = operand2;
 };
 displayOperand2();
 
 // Converts operand2 from a string into a valid float
-const parser = function () {
-  let parsed;
-  if (operand2.slice(0, 1) == "-") parsed = -parseFloat(operand2.slice(1));
-  else parsed = parseFloat(operand2);
-  return parsed;
-};
 
 /* Performs all of the operations of the calculator:
    addition, subtraction, multiplication, division, clear, and equals/enter
@@ -33,29 +25,29 @@ const parser = function () {
 const operation = function () {
   switch (operator) {
     case "":
-      operand1 = parser() || operand1;
+      operand1 = parseFloat(operand2) || operand1;
       displayOperand1();
-      operand2 = "0";
+      operand2 = "";
       break;
     case "+":
-      operand1 += parser();
+      operand1 += parseFloat(operand2) || 0;
       displayOperand1();
-      operand2 = "0";
+      operand2 = "";
       break;
     case "-":
-      operand1 -= parser();
+      operand1 -= parseFloat(operand2) || 0;
       displayOperand1();
-      operand2 = "0";
+      operand2 = "";
       break;
     case "*":
-      operand1 *= parser() || 1;
+      operand1 *= parseFloat(operand2) || 1;
       displayOperand1();
-      operand2 = "0";
+      operand2 = "";
       break;
     case "/":
-      operand1 /= parser() || 1;
+      operand1 /= parseFloat(operand2) || 1;
       displayOperand1();
-      operand2 = "0";
+      operand2 = "";
       break;
     default:
       handlers.clear();
@@ -64,47 +56,10 @@ const operation = function () {
 
 // Takes all the button presses, and changes and executes what needs to be executed
 const handlers = {
-  numericInputZero: function () {
-    operand2 += "0";
+  numericInput: function (value) {
+    operand2 += `${value}`;
     displayOperand2();
   },
-  numericInputOne: function () {
-    operand2 += "1";
-    displayOperand2();
-  },
-  numericInputTwo: function () {
-    operand2 += "2";
-    displayOperand2();
-  },
-  numericInputThree: function () {
-    operand2 += "3";
-    displayOperand2();
-  },
-  numericInputFour: function () {
-    operand2 += "4";
-    displayOperand2();
-  },
-  numericInputFive: function () {
-    operand2 += "5";
-    displayOperand2();
-  },
-  numericInputSix: function () {
-    operand2 += "6";
-    displayOperand2();
-  },
-  numericInputSeven: function () {
-    operand2 += "7";
-    displayOperand2();
-  },
-  numericInputEight: function () {
-    operand2 += "8";
-    displayOperand2();
-  },
-  numericInputNine: function () {
-    operand2 += "9";
-    displayOperand2();
-  },
-
   decimal: function () {
     operand2 += ".";
     displayOperand2();
@@ -134,14 +89,11 @@ const handlers = {
   },
   clear: function () {
     operand1 = 0.0;
-    operand2 = "0";
+    operand2 = "";
     operator = "";
     displayOperand2();
   },
   enter: function () {
-    if (operand2 == 0 && operator == "") {
-      operand2 == operand1;
-    }
     operation();
     operator = "";
   },
